79937c1eca0f9aa9415f4fe5c1666a2e048bc890
Rosa <rosa.meijers@karify.com>
2023-06-28 18:01:06 +0200

    Reload list with updated data from API (wip)

--- Crypto/Core/Home/ViewModels/HomeViewModel.swift
+++ Crypto/Core/Home/ViewModels/HomeViewModel.swift
@@ -45,37 +45,11 @@ class HomeViewModel: ObservableObject {
             }
             .store(in: &cancellables)
         
-        // updates market data
-        // download data, decode it in market data model, put it into statistics view
-        marketDataService.$marketData // variable that is set after downloading
-            .map { (marketDataModel) -> [StatisticModel] in
-                var stats: [StatisticModel] = []
-                
-                guard let data = marketDataModel else { return stats }
-                
-                let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
-                
-                let volume = StatisticModel(title: "24h Volume", value: data.volume)
-                
-                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
-                
-                let portfolio = StatisticModel(title: "Portfolio Value", value: "$0.00", percentageChange: 0)
-                
-                stats.append(contentsOf: [marketCap, volume, btcDominance, portfolio])
-                
-                return stats
-                        
-            }
-            .sink { [weak self] (returnedStats) in
-                self?.statistics = returnedStats
-            }
-            .store(in: &cancellables)
-        
         // updates portfolio
         // we want to convert the portfoliocoins to coinmodels so we can add them to the coin model array
         // therefore we want to subscribe to allcoins and portfoliocoins
         $allCoins // take latest array of filtered coins of type coin model
-            .combineLatest(portfolioDataService.$savedEntities) // type portfolio entity
+            .combineLatest(portfolioDataService.$savedEntities) // type portfolio entity, save the coins in core data
             .map { (coinmodels, portfolioEntities) -> [CoinModel] in
                 coinmodels
                     .compactMap { (coin) -> CoinModel? in
@@ -89,10 +63,55 @@ class HomeViewModel: ObservableObject {
                 self?.portfolioCoins = returnedCoins
             }
             .store(in: &cancellables)
+        
+        // updates market data
+        // download data, decode it in market data model, put it into statistics view
+        marketDataService.$marketData // variable that is set after downloading
+            .combineLatest($portfolioCoins) // every time the portfolioCoins is updated, these functions will re-run
+            .map(mapMarketData)
+            .sink { [weak self] (returnedStats) in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
     }
     
     func updatePortfolio(coin: CoinModel, amount: Double) {
         portfolioDataService.updatePortfolio(coin: coin, amount: amount)
+    }
+    
+    private func mapMarketData(marketDataModel: MarketDataModel?, portfolioCoins: [CoinModel]) -> [StatisticModel] {
+        var stats: [StatisticModel] = []
+        
+        guard let data = marketDataModel else { return stats }
+        
+        let marketCap = StatisticModel(title: "Market Cap", value: data.marketCap, percentageChange: data.marketCapChangePercentage24HUsd)
+        
+        let volume = StatisticModel(title: "24h Volume", value: data.volume)
+        
+        let btcDominance = StatisticModel(title: "BTC Dominance", value: data.btcDominance)
+        
+        let portfolioValue =
+            portfolioCoins
+            .map({ $0.currentHoldingsValue})
+            .reduce(0, +) // instead of array of Double we want one total number of portfolio value, so use reduce
+        
+        let previousValue =
+            portfolioCoins
+            .map { (coin) -> Double in
+                let currentValue = coin.currentHoldingsValue
+                let percentChange = coin.priceChangePercentage24H ?? 0 / 100
+                let previousValue = currentValue / (1 + percentChange)
+                return previousValue
+            }
+            .reduce(0, +) // you want one single previousValue, so sum up
+        
+        let percentageChange = ((portfolioValue - previousValue) / previousValue) * 100
+        
+        let portfolio = StatisticModel(title: "Portfolio Value", value: portfolioValue.asCurrencyWith2Ddecimals(), percentageChange: percentageChange)
+        
+        stats.append(contentsOf: [marketCap, volume, btcDominance, portfolio])
+        
+        return stats
     }
     
     private func filteredCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
